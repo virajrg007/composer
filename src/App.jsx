74720b9b0635eda0da707eb9f8300ec9c60f305c
VirajRG <viraj@spaceuptech.com>
2019-03-08 23:12:06 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import React, {Component} from 'react';
-import {Router, Route} from 'react-router-dom';
-import {connect} from 'react-redux';
-
-import history from './history'
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
-      <div></div>
-      </Router>
+      <div>Music Composer</div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
-import './css/app.css';
 
-import store from './store'
-
-// store.dispatch(connect('ws://localhost:8080/v1/json/socket', true));
-ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
